d603419d9dc84db065afe34c106d833dda3bac6d
Christopher Neugebauer <chris.neugebauer@alphasights.com>
2019-07-26 12:58:23 -0700

    Import the REST extensions from application repository

---
+++ src/main/kotlin/TwilioRestExtensions.kt
@@ -0,0 +1,9 @@
+package com.alphasights.kotlintwilio
+
+import com.twilio.base.*
+
+fun <T, U: Deleter<T>> U.deleteWith(f: U.() -> Unit) = this.apply(f).delete()
+fun <T, U: Creator<T>> U.createWith(f: U.() -> Unit): T  = this.apply(f).create()
+fun <T, U: Fetcher<T>> U.fetchWith(f: U.() -> Unit): T = this.apply(f).fetch()
+fun <T: Resource, U: Reader<T>> U.readWith(f: U.() -> Unit): ResourceSet<T> = this.apply(f).read()
+fun <T, U: Updater<T>> U.updateWith(f: U.() -> Unit): T = this.apply(f).update()
